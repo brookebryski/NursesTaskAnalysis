e465a6e81c2e3c4f9bb4902a15e4aa8639a7768a
dooster <dustin2burns@yahoo.com>
2022-05-26 09:22:39 -0500

    added basica html templates for user interfaces using gin

--- main.go
+++ main.go
@@ -1,39 +1,9 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	routePack "NurseTasks/router"
 )
 
 func main() {
-	router := gin.Default()
-	router.LoadHTMLGlob("template/*")
-	// http.Handle("/login/", http.FileServer(http.Dir("./nurseLogin")))
-	// fmt.Println("Hello World")
-	router.GET("/login/Nurses", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "nurseLogin.html", gin.H{
-			"content": "This is a login page...",
-		})
-	})
-	router.GET("/login/Supervisors", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "supLogin.html", gin.H{
-			"content": "This is a login page...",
-		})
-	})
-	router.GET("/tasks/input", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "nurseTasks.html", gin.H{
-			"content": "This the Nurses Task Page...",
-		})
-	})
-	router.GET("/review/tasks", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "supTasksView.html", gin.H{
-			"content": "This is the supervisors View Page...",
-		})
-	})
-	router.Run("localhost:3000")
+	routePack.Routing()
 }
-
-// func loginViewHandler(w http.ResponseWriter, r *http.Request) {
-// 	render.HTML("nurseLogin.html")
-// }
