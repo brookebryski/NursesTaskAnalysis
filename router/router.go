6fc2ca5219ea6aceb3909037e9e1a32608777f97
dooster <dustin2burns@yahoo.com>
2022-06-03 12:18:36 -0500

    added notes to code

--- router/router.go
+++ router/router.go
@@ -20,17 +20,17 @@ var tpl *template.Template
 
 func Routing() {
 	tpl = template.Must(template.ParseGlob("templates/*.html"))
-	http.HandleFunc("/nurselogin", getNurseFormHandler)
-	http.HandleFunc("/nursetasks", loginauthFormHandler)
-	http.HandleFunc("/tasks", tasksHandler)
-	http.HandleFunc("/supervisorlogin", getSupFormHandler)
-	http.HandleFunc("/suphome", getSupHomeHandler)
-	http.HandleFunc("/", postTask)
-	http.HandleFunc("/getenteredtasks", getEnteredTasks)
-	http.HandleFunc("/getfacilitiesdropdowndatabyregion", getFacilitiesDropdownDataByRegion)
-	http.HandleFunc("/getfacilitiestabledatabyregion", getFacilitiesTableDataByRegion)
-	http.HandleFunc("/createnewtask", createNewTasks)
-	http.HandleFunc("/getregionandfacility", getregionandfacilityHandler)
+	http.HandleFunc("/nurselogin", getNurseFormHandler)                                      //login page for nurses.
+	http.HandleFunc("/nursetasks", loginauthFormHandler)                                     //nurses task/home page. Validation occurs here
+	http.HandleFunc("/tasks", tasksHandler)                                                  //nurse page, populates the auto_select dropdown for submit a task form
+	http.HandleFunc("/supervisorlogin", getSupFormHandler)                                   //login page for supervisor
+	http.HandleFunc("/suphome", getSupHomeHandler)                                           //home page for supervisor, validation occurs here
+	http.HandleFunc("/", postTask)                                                           //for nurses page, creates a new task assigned to a department
+	http.HandleFunc("/getenteredtasks", getEnteredTasks)                                     //for nurse page, gets the task table for completed tasks
+	http.HandleFunc("/getfacilitiesdropdowndatabyregion", getFacilitiesDropdownDataByRegion) //for supervisors, populates dropdown for facilities based on selected region dropdown
+	http.HandleFunc("/getfacilitiestabledatabyregion", getFacilitiesTableDataByRegion)       //cut from final view
+	http.HandleFunc("/createnewtask", createNewTasks)                                        //for nurses, takes a modal of whatever the nurse enters and creates a new task that can be submitted.
+	http.HandleFunc("/getregionandfacility", getregionandfacilityHandler)                    //grabs the region Name of the user and the facility Name of the user and displays it on information
 	http.ListenAndServe(":3030", nil)
 }
 
@@ -53,7 +53,6 @@ func loginauthFormHandler(w http.ResponseWriter, r *http.Request) {
 				tpl.ExecuteTemplate(w, "nurseTasks.html", user)
 
 			} else {
-
 				tpl.ExecuteTemplate(w, "nurseLogin.html", "You dont have access because you are not a Nurse")
 				return
 			}
@@ -167,13 +166,13 @@ func getEnteredTasks(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func getFacilitiesDropdownDataByRegion(w http.ResponseWriter, r *http.Request){
+func getFacilitiesDropdownDataByRegion(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	type Facility struct {
-		ID         uint   `gorm:"not null"`
-		RegionID   uint   `gorm:"not null"`
-		Name       string `gorm:"not null"`
+		ID       uint   `gorm:"not null"`
+		RegionID uint   `gorm:"not null"`
+		Name     string `gorm:"not null"`
 	}
 	var facilities []Facility
 	id := r.URL.Query().Get("id")
@@ -195,9 +194,9 @@ func getFacilitiesTableDataByRegion(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	type Facility struct {
-		ID         uint   `gorm:"not null"`
-		RegionID   uint   `gorm:"not null"`
-		Name       string `gorm:"not null"`
+		ID       uint   `gorm:"not null"`
+		RegionID uint   `gorm:"not null"`
+		Name     string `gorm:"not null"`
 	}
 	var facilities []Facility
 	id := r.URL.Query().Get("id")
@@ -212,7 +211,7 @@ func getFacilitiesTableDataByRegion(w http.ResponseWriter, r *http.Request) {
 
 	}
 }
-	
+
 func createNewTasks(w http.ResponseWriter, r *http.Request) {
 	var t models.Task
 	w.WriteHeader(http.StatusCreated)
